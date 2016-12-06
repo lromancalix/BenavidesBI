0ab142b6a99cefc5466c185b78b771bd9fdb966a
lromancalix <roman.calix.net@gmail.com>
2016-12-06 00:08:07 -0600

    Agregar archivos de proyecto.

---
+++ Benavides.WEB/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Benavides.WEB.Startup))]
+namespace Benavides.WEB
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
